d7ac9619f0578a38db780c0eba275586d1e2b3c3
Amitrana001-igl <amit2005144@gmail.com>
2026-02-19 09:55:09 +0530

    1hr

--- src/Array/MinimumValue.java
+++ src/Array/MinimumValue.java
@@ -1,22 +1,24 @@
 package Array;
+
 import java.util.Scanner;
 
-public class SumOfElement {
+public class MinimumValue {
     public static void main(String[] args){
-
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter ur elements: ");
+
+        System.out.println("Enter the number of elements: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-        System.out.print("enter the numbers: ");
-        for(int i=0;i<n;i++){
+        System.out.print("Enter the elements: ");
+        for(int i =0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        int sum = 0;
+        int min = arr[0];
         for(int i=0;i<n;i++){
-            sum+= arr[i];
+            if(arr[i]<min) min = arr[i];
         }
-        System.out.println(sum);
+        System.out.print(min);
+
     }
 }
