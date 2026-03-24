ed82655fa2c6d9659c3b66a9b31cbeb0afa788ca
Amitrana001-igl <amit2005144@gmail.com>
2026-03-24 13:55:28 +0530

    re

--- src/Array/ArrayBasics.java
+++ src/Array/ArrayBasics.java
@@ -2,15 +2,20 @@ package Array;
 
 public class ArrayBasics {
     public static void main(String[] args){
-//        int[] x ={2,3,4,5,6,3,2,1};
-//        //indexing
-//        System.out.println(x[3]);
-//        //updating elements
-//        x[3]= 89;
-//        System.out.println(x[3]);
+//         int[] arr = {1,2,34,5,4,};
+//
+//         //indexing
+//        System.out.println(arr[2]);
+//
+//        //updating elements -- mutable
+//        arr[2] = 2;
+//        System.out.println(arr[2]);
 
-        int[] arr = new int[4]; // 4elements ka array
+        int[] a = new int[3];
+        a[0] = 2;
+        a[1] = 12;
+        a[2] = 3;
 
-
+        System.out.println(a[2]);
     }
 }
